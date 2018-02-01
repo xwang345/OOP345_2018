f7c8d4b7c6283a28d97d45108c96f9df1a7c2179
Xiaochen Wang (Ryan) <ryan.wang23@hotmail.com>
2018-01-31 20:23:37 -0500

    Update week1.cpp

--- week1.cpp
+++ week1.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-	cout.setf(ios::showpoint);
-	cout.setf(ios::showpos);
-	cout.precision(3);
-	cout.setf(ios::fixed, ios::floatfield);
-	/*cout.setf(ios::internal, ios::adjustfield);
-	cout.width(10);*/
-	cout << 275.5 << endl;
+    cout.setf(ios::showpoint);
+    cout.setf(ios::showpos);
+    cout.precision(3);
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.setf(ios::internal, ios::adjustfield);
+    cout.width(10);
+    cout << 275.5 << endl;
 	
-	cout << setw(5) << setprecision(2) << 1.2345 << setw(10)
-		<< setprecision(4) << sqrt(2) << setw(15) << setiosflags(ios::scientific)
-		<< sqrt(3) << endl;
+    cout << setw(5) << setprecision(2) << 1.2345 << setw(10)
+         << setprecision(4) << sqrt(2) << setw(15) << setiosflags(ios::scientific)
+         << sqrt(3) << endl;
 
-	cout << fixed << setprecision(2) << 123.456 << endl;
+    cout << fixed << setprecision(2) << 123.456 << endl;
     return 0;
 }
