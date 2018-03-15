49dc5ec66f4a0e6da1afc0722728474c78c8c3a3
Xiaochen Wang (Ryan) <ryan.wang23@hotmail.com>
2018-03-14 20:42:35 -0400

    Update OOP345_A2.cpp

--- OOP345_A2.cpp
+++ OOP345_A2.cpp
@@ -8,10 +8,10 @@ class Radius
 	friend void Show_r(Radius &n);     //declare Show_r is friend function
 
 public:
-	Radius(int x){
+	Radius(int x) {
 		r = x;
 	}
-	~Radius(){
+	~Radius() {
 
 	}
 
@@ -19,17 +19,19 @@ private:
 	int r;
 };
 
-void Show_r(Radius &n){
-	cout << "Circle's radius: " << n.r << endl;    //call Radius prviate data member
+void Show_r(Radius &n) {
+	cout << endl;
+	cout << "******************************************" << endl;
+	cout << "Circle's radius   : " << n.r << endl;    //call Radius prviate data member
 }
 
-class Circle{
+class Circle {
 public:
 	Circle() {}
 	~Circle() {}
 
-	double area(Radius a){
-		s = a.r * a.r * 3.1415926;             
+	double area(Radius a) {
+		s = a.r * a.r * 3.1415926;
 		return s;
 	}
 
@@ -37,29 +39,33 @@ private:
 	double s;
 };
 
-class Sphere{
-    public:
-    Sphere(){}
-    ~Sphere(){}
-    
-    double volume(Radius a){
-        s = a.r * a.r * a.r *3.1415926 * 4/3;
-        return s;
-    }
-    private:
-    double s;
+class Sphere {
+public:
+	Sphere() {}
+	~Sphere() {}
+
+	double volume(Radius a) {
+		s = a.r * a.r * a.r *3.1415926 * 4 / 3;
+		return s;
+	}
+private:
+	double s;
 };
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 	Radius objRadius(9);
 	Circle objCircle;
 	Sphere objSphere;
-	
+
 
 	Show_r(objRadius);
-	
-	cout << "Circle area is：" << objCircle.area(objRadius) << endl;
-	cout << "Sphere volume is: " << objSphere.volume(objRadius) << endl;
+
+	cout << "Circle's area     : " << objCircle.area(objRadius) << endl;
+	cout << "Sphere's volume is: " << objSphere.volume(objRadius) << endl;
+	cout << "******************************************" << endl;
+
+
+	system("pause");
 
 	return 0;
 }
